72e2fc78acb8ec65d352c3e672ef0e4c79c02042
Olga <888yalosinger@ukr.net>
2024-06-13 12:26:59 -0500

    "config path change and page name change"

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-resume-page/",
-  plugins: [react()],
+    base: '/',
+    plugins: [react()],
 });
